302dfa5ce95738cc325aed2ddd0c771d01dd0324
laecio22 <laecon22@hotmail.com>
2025-03-08 19:03:06 -0300

    style: inicio  estilização parte  de bem-vindo

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,37 @@
-import styles from './style.module.css';
-import Image from 'next/image';
-import imageTecnologia from '../public/assets/imagens/home.png'
+import styles from "./style.module.css";
+import Image from "next/image";
+import imageTecnologia from "../public/assets/imagens/home.png";
+import { PiHandWavingDuotone } from "react-icons/pi";
 export default function Home() {
   return (
-   <section className={styles.containerPrincipal}>
-         
+    <section className={styles.containerPrincipal}>
       <section className={styles.sectionHome}>
-        <div>
-          <div>Olá , o meu nome é </div>
-          <div>Laécio</div>
-          <div>Desenvolvedor frontend</div>
-          <button>Conheça meu portifólio</button>
+        <div className={styles.containerText}>
+          <div className={styles.subContainerText}>
+            <div className={styles.welcome}>
+              <span className={styles.spanWave}>
+                <PiHandWavingDuotone className={styles.iconHandle} />
+              </span>
+              Olá , o meu nome é{" "}
+            </div>
+            <h1 className={styles.name}>Laécio</h1>
+            <h3 className={styles.cargo}>Desenvolvedor frontend</h3>
+            <button className={styles.button}>Conheça meu portifólio</button>
+          </div>
         </div>
         <div className={styles.containerImage}>
-           <Image src={imageTecnologia} alt='ilustração  de  uma pessoa  segurando um notebook' className={styles.imageUser}/>
+          <Image
+            src={imageTecnologia}
+            alt="ilustração  de  uma pessoa  segurando um notebook"
+            className={styles.imageUser}
+          />
         </div>
       </section>
-      <section>
-        section  apresentação
-      </section>
+      <section>section apresentação</section>
       <section>Section habilidades</section>
       <section>Section Experiências</section>
       <section>Section Projetos</section>
       <section>Section Contato</section>
-   </section>
-    
+    </section>
   );
 }
