1a5e18a0cf4cfb2839d488247458df9759aebec7
laecio22 <laecon22@hotmail.com>
2025-09-04 16:21:08 -0300

    style:conclusão  estilização  sessão experiências  e  alteração  conteúdo  sessão apresentação

--- app/page.tsx
+++ app/page.tsx
@@ -7,58 +7,57 @@ import { FaArrowRight } from "react-icons/fa6";
 import { IoIosSend } from "react-icons/io";
 import Link from "next/link";
 import CardSkill from "./components/CardSkill/CardSkill";
-import imgHtml from '../public/assets/imagens/iconHtml.png';
-import imgCss  from '../public/assets/imagens/iconCss.png';
-import imgJavascript  from '../public/assets/imagens/iconJavascript.png';
-import imgTypescript  from '../public/assets/imagens/iconTypescript.png';
-import imgNext  from '../public/assets/imagens/iconNext.png';
-import imgTailwind  from '../public/assets/imagens/iconTailwind.png';
-import imgStyled  from '../public/assets/imagens/styled-components.svg';
-import imgGit  from '../public/assets/imagens/iconGit.png';
-import imgVue  from '../public/assets/imagens/iconVue.png';
+import imgHtml from "../public/assets/imagens/iconHtml.png";
+import imgCss from "../public/assets/imagens/iconCss.png";
+import imgJavascript from "../public/assets/imagens/iconJavascript.png";
+import imgTypescript from "../public/assets/imagens/iconTypescript.png";
+import imgNext from "../public/assets/imagens/iconNext.png";
+import imgTailwind from "../public/assets/imagens/iconTailwind.png";
+import imgStyled from "../public/assets/imagens/styled-components.svg";
+import imgGit from "../public/assets/imagens/iconGit.png";
+import imgVue from "../public/assets/imagens/iconVue.png";
 
 export default function Home() {
-
   const skills = [
     {
-      title: 'HTML',
-      src: imgHtml
+      title: "HTML",
+      src: imgHtml,
     },
     {
-      title: 'Javascript',
-      src:imgJavascript
+      title: "Javascript",
+      src: imgJavascript,
     },
     {
-      title: 'CSS',
-      src:imgCss
+      title: "CSS",
+      src: imgCss,
     },
     {
-      title: 'Typescript',
-      src:imgTypescript
+      title: "Typescript",
+      src: imgTypescript,
     },
     {
-      title: 'Tailwind',
-      src:imgTailwind
+      title: "Tailwind",
+      src: imgTailwind,
     },
     {
-      title: 'Styled-Components',
-      src: imgStyled
+      title: "Styled-Components",
+      src: imgStyled,
     },
     {
-      title: 'Next',
-      src: imgNext
+      title: "Next",
+      src: imgNext,
     },
     {
-      title: 'Git',
-      src: imgGit
+      title: "Git",
+      src: imgGit,
     },
     {
-      title: 'Vue',
-      src: imgVue
+      title: "Vue",
+      src: imgVue,
     },
-  ]
+  ];
   return (
-    <section className='container'>
+    <section className="container">
       <section className={styles.sectionHome}>
         <div className={styles.containerText}>
           <div className={styles.subContainerText}>
@@ -95,100 +94,110 @@ export default function Home() {
         <div className={styles.containerTextApresentation}>
           <h2 className={styles.subTitleApresentation}> Deixe-me apresentar</h2>
           <div>
-            Sou um desenvolvedor front-end apaixonado por projetar e codificar.
-            Minha especialidade é a criação de interfaces e funcionalidades para
-            aplicativos da web usando React.js e Next.js. Você pode conhecer
-            mais sobre o meu perfil e o meu trabalho navegando pelo website.
+             <p>Sou  Laécio, natural  de Codó-MA, mas  atualmente  moro  em Altos-PI,
+            sou um desenvolvedor front-end apaixonado por projetar e codificar.</p>
+            <p> Minha especialidade é a criação de interfaces e funcionalidades para
+            aplicativos da web usando React.js.</p>
+            <p> Meu  primeiro contato com  desenvolvimento web,
+            foi no curso técnico  em informática no IFPI, onde  tive  a oportunidade  de aprender
+            as  bases  do  desenvolvimento  web,  como  HTML, CSS e JavaScript.</p>
+            <p> Desde então, venho me dedicando a aprimorar minhas habilidades e
+            conhecimentos na área, buscando sempre estar atualizado com as
+            últimas tendências e tecnologias do mercado e  buscar sempre resolver  os  problemas.
+            Você pode conhecer mais sobre o meu perfil e o meu trabalho navegando pelo website.</p>      
             <p> P.S. este site é código aberto e está disponível no Github</p>
-          </div>        
+          </div>
           <Link href="/contato">
             Entre em Contato <IoIosSend className={styles.iconSend} />
           </Link>
         </div>
       </section>
       <section className={styles.sectionSkills}>
-          <h1>Habilidades</h1>
-          <div className={`${styles.containerSkills} container`}>
-            {skills.map((skill, index)=> {
-              return (
-                <CardSkill key={index} title={skill.title} imgSkill={skill.src}/>
-              )
-            })}
-          </div>
+        <h1>Habilidades</h1>
+        <div className={`${styles.containerSkills} container`}>
+          {skills.map((skill, index) => {
+            return (
+              <CardSkill key={index} title={skill.title} imgSkill={skill.src} />
+            );
+          })}
+        </div>
       </section>
       <section className={styles.sectionExperiences}>
         <h1>Experiências</h1>
         <div className={`${styles.containerExperiences} container`}>
-            <div className={styles.cardExperiences}>
-              <h2>MSB Tecnologia ltda</h2>
-              <span>2022-2024</span>
-              <p>Desenvolvedor Frontend</p>
-              <p>Atividades desenvolvidas:</p>
-              <ul>
-                <li>Desenvolvimento de interfaces web</li>
-                <li>Manutenção de sistemas legados</li>
-                <li>Colaboração com equipes de design e backend</li>
-                <li>Participação das  reuniões de planning,  dailys e reviews </li>
-              </ul>
-              <p>Principais stacks utilizadas:</p>
-              <ul>
-                <li>React.js</li>                
-                <li>Styled-Components</li>
-                <li>Tailwindcss</li>
-                <li>Git/GitHub</li>
-                <li>Typescript</li>
-                <li>JavaScript</li>
-                <li>HTML5 e CSS3</li> 
-                <li>Vue</li>
-              </ul>
-            </div>
-            <div className={styles.cardExperiences}>
-              <h2>MSB Tecnologia ltda</h2>
-              <span>2022-2024</span>
-              <p>Desenvolvedor Frontend</p>
-              <p>Atividades desenvolvidas:</p>
-              <ul>
-                <li>Desenvolvimento de interfaces web</li>
-                <li>Manutenção de sistemas legados</li>
-                <li>Colaboração com equipes de design e backend</li>
-                <li>Participação das  reuniões de planning,  dailys e reviews </li>
-              </ul>
-              <p>Principais stacks utilizadas:</p>
-              <ul>
-                <li>React.js</li>                
-                <li>Styled-Components</li>
-                <li>Tailwindcss</li>
-                <li>Git/GitHub</li>
-                <li>Typescript</li>
-                <li>JavaScript</li>
-                <li>HTML5 e CSS3</li> 
-                <li>Vue</li>
-              </ul>
-            </div>
-            <div className={styles.cardExperiences}>
-              <h2>MSB Tecnologia ltda</h2>
-              <span>2022-2024</span>
-              <p>Desenvolvedor Frontend</p>
-              <p>Atividades desenvolvidas:</p>
-              <ul>
-                <li>Desenvolvimento de interfaces web</li>
-                <li>Manutenção de sistemas legados</li>
-                <li>Colaboração com equipes de design e backend</li>
-                <li>Participação das  reuniões de planning,  dailys e reviews </li>
-              </ul>
-              <p>Principais stacks utilizadas:</p>
-              <ul>
-                <li>React.js</li>                
-                <li>Styled-Components</li>
-                <li>Tailwindcss</li>
-                <li>Git/GitHub</li>
-                <li>Typescript</li>
-                <li>JavaScript</li>
-                <li>HTML5 e CSS3</li> 
-                <li>Vue</li>
-              </ul>
-            </div>
-        
+          <div className={styles.cardExperiences}>
+            <h2>MSB Tecnologia ltda</h2>
+            <span>2022-2024</span>
+            <p>Desenvolvedor Frontend</p>
+            <p>Atividades desenvolvidas:</p>
+            <ul>
+              <li>
+                Definição do layout dos aplicativos e sites desenvolvidos,
+                criando uma experiência agradável e intuitiva para os usuários.
+              </li>
+              <li> Implementação layout seguindo o protótipo. </li>
+              <li>Correção de bugs. </li>
+              <li>Participação das reuniões de planning, dailys e review. </li>
+              <li>
+                Manutenção e atualização dos sistemas e aplicações, assegurando
+                o bom funcionamento.
+              </li>
+              <li>
+                Implementação de novas funcionalidades e melhorias nos sistemas
+                existentes, buscando aprimorar a experiência do usuário.
+              </li>
+            </ul>
+            <p>Principais stacks utilizadas:</p>
+            <ul>
+              <li>React.js</li>
+              <li>Styled-Components</li>
+              <li>Tailwindcss</li>
+              <li>Git/GitHub</li>
+              <li>Typescript</li>
+              <li>JavaScript</li>
+              <li>HTML5 e CSS3</li>
+              <li>Vue</li>
+            </ul>
+          </div>
+          <div className={styles.cardExperiences}>
+            <h2>Zukk Tecnologia ltda</h2>
+            <span>2021-2022</span>
+            <p>Desenvolvedor Frontend</p>
+            <p>Atividades desenvolvidas:</p>
+            <ul>
+              <li>Desenvolvimento de interfaces web</li>
+              <li>Manutenção de sistemas legados</li>
+              <li>Colaboração com equipes de design e backend</li>
+              <li>Participação das reuniões de planning, dailys e reviews </li>
+            </ul>
+            <p>Principais stacks utilizadas:</p>
+            <ul>
+              <li>React.js</li>
+              <li>Git/GitHub</li>
+              <li>Typescript</li>
+              <li>JavaScript</li>
+              <li>HTML5 e CSS3</li>
+            </ul>
+          </div>
+          <div className={styles.cardExperiences}>
+            <h2>Tribunal Regional Eleitoral do Piauí</h2>
+            <span>2018-2019</span>
+            <p>Operador de Suporte Técnico</p>
+            <p>Atividades desenvolvidas:</p>
+            <ul>
+              <li>Suporte ao usuário.</li>
+              <li> Instalação e Desinstalação de programas e softwares.</li>
+              <li>               
+                Instalação e Desinstalação dos computadores, impressoras e
+                outros equipamentos de hardware.
+              </li>
+              <li>
+                Prestação de atendimento técnico, acionando a equipe operacional
+                quando necessário, conforme as normas e procedimentos da
+                empresa, visando a solução do problema do cliente.
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
       <section>Section Projetos</section>
